952563dad10b804fb42c2d61ef157505a5154202
pedrongit <pedronascimento98@gmail.com>
2021-11-06 12:26:53 -0300

    Sem ROHS

--- cod_pedro/cod_pedro.ino
+++ cod_pedro/cod_pedro.ino
@@ -1,90 +1,49 @@
 
-#include <ros.h>
-#include <std_msgs/Int8.h>  
-
+#include  <LiquidCrystal.h>
 
 //-------------------------------------------------------------------------- 
-  #define rele1 1
+  #define R1 1
   #define amostras 12 
   #define Vref 5
   #define Divisor 2.61
 //---------------------------------------------------------------------------
+
+float medicao(const uint8_t);
+void printlcd(int, int , float, int);
+
 void setup() {    //(◑_◑)
   Serial.begin(9600);
-  pinMode(rele1, OUTPUT); 
-  digitalWrite(rele1,HIGH);  
-  std_msgs::Int8 msg;
-  
+  pinMode(R1, OUTPUT); 
+  digitalWrite(R1,HIGH);  
 }
 
-float medicaoA0(){
-  float Val= analogRead (A0), total =0;
-  for (int i = 0; i < amostras; i++){
-    total = total + Val*Vref*Divisor/1024 ;
-  }
-  float medicao = total/amostras;
-  Serial.print("      A0:");
-  Serial.print(medicao);
-  return medicao;
-}
-float medicaoA1(){
-  float Val= analogRead (A1),  total =0;
+
+float medicao(const uint8_t pino){
+  float Val, total;
+  total = 0;
+  Val = analogRead(pino);
   for (int i = 0; i < amostras; i++){
     total = total + Val*Vref*Divisor/1023 ;
   }
-  float medicao = total/amostras;
-  Serial.print("      A1:");
-  Serial.print(medicao);
-  return medicao;
+  return total/amostras;
 }
-float medicaoA2(){
-  float Val= analogRead (A2), total =0;
+
+//---------------------------------------------------------------------------
+//---------------------------------------------------------------------------
+void loop(){
+
+}
+
+float medicao(const uint8_t pino){
+  float Val, total;
+  total = 0;
+  Val = analogRead(pino);
   for (int i = 0; i < amostras; i++){
     total = total + Val*Vref*Divisor/1023 ;
   }
-  float medicao = total/amostras;
-  Serial.print("      A2:");
-  Serial.println(medicao);
-  return medicao;
+  return total/amostras;
 }
 
-void ifrele1(){
-  float VA0 = medicaoA0();
-  if (VA0<12.8){
-    digitalWrite(rele1,LOW);
-  }
+void printlcd(int coluna, int linha, float medicao, int bateria){
+  lc
 }
-//---------------------------------------------------------------------------
-//---------------------------------------------------------------------------
- int main() {
-   
-  
- ros::NodeHandle n;
-
-int count = 0;
-while (ros::ok())
- {
-
-std_msgs::Int8 msg;
-std::stringstream ss;
-
-void loop() {// __________________  t(>.<t)
-  medicaoA0();
-  medicaoA1();
-  medicaoA2();
-  
-  delay(1000);
-}
-
-  ros::init(medicao, "talker");
-  ros::Publisher chatter_pub = n.advertise<std_msgs::Int8>("chatter", 1000); //tipo de mensagem e seu tamanho
-  ros::Rate loop_rate(10);  //frequencia do loop
-
-  ROS_INFO("%d", medicao);
-  chatter_pub.publish(medicao);
-  ros::spinOnce();
-  loop_rate.sleep();
-
- }
- return 0;
-  }
